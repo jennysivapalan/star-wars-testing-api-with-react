5756c6da0d2bcf1f4830c8aa52d20f4fc037f3e4
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-12-15 14:30:15 +0000

    feat: calls the starwars API and renders the first name in the list for the people endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import darthLogo from "./assets/darth.svg";
 import "./App.css";
 
 function App() {
+  const endpoint = "https://swapi.dev/api/people";
+  const [name, setName] = useState<string>("aaa");
+
+  useEffect(() => {
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((obj) => setName(obj.results[0].name))
+      .catch((error) => console.error("Error", error));
+  }, []);
+
   return (
     <>
       <div>
@@ -12,6 +22,7 @@ function App() {
       </div>
       <h1>Testing API calls in React with the Star Wars API </h1>
       <div className="card"></div>
+      <p>{name}</p>
     </>
   );
 }
